285566388219eb3e603a9b3d8a6d8931116cd7ea
Danny Lowater 🫶 <git@dannythomas.dev>
2024-07-12 09:08:00 +0100

    feat: add basic dynamic text

--- src/components/typingText.tsx
+++ src/components/typingText.tsx
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react"
 import { cn } from "../lib/utils"
 
-const Cursor = ({ blink }: { blink?: boolean }) => (
+const Cursor = ({ blink, withSpace }: { blink?: boolean; withSpace?: boolean }) => (
 	<span style={{ fontFamily: "default" }} className={cn(blink && "animate-blink", "font-normal")}>
-		{" |"}
+		{withSpace ? " |" : "|"}
 	</span>
 )
 
-/** TypingText component that types out the given text */
-export const TypingText = ({
-	text,
-	startTyping,
-	onFinish,
-	isFinished,
-}: {
+const typingSpeed = 60
+const backspaceSpeed = 60
+
+type TypingTextProps = {
 	text: string
 	startTyping: boolean
 	onFinish: () => void
 	isFinished: boolean
-}) => {
+}
+
+/** TypingText component that types out the given text */
+export const TypingText = ({ text, startTyping, onFinish, isFinished }: TypingTextProps) => {
 	const [currentText, setCurrentText] = useState("")
 	const [blink, setBlink] = useState(false)
 	const [isTyping, setIsTyping] = useState(false)
@@ -26,10 +26,13 @@ export const TypingText = ({
 	useEffect(() => {
 		if (!startTyping) return
 		setIsTyping(true)
+
+		// every 0.075 seconds type another character
 		const interval = setInterval(() => {
 			setCurrentText((prev) => prev + text[prev.length])
-		}, 75)
+		}, typingSpeed)
 
+		// Finished typing, let the cursor blink for 2 seconds
 		if (currentText.length === text.length) {
 			clearInterval(interval)
 			setBlink(true)
@@ -45,7 +48,105 @@ export const TypingText = ({
 	return (
 		<span>
 			{isFinished ? text : currentText}
-			{isTyping && <Cursor blink={blink} />}
+			{isTyping && <Cursor withSpace blink={blink} />}
 		</span>
+	)
+}
+
+type BackspacingTextProps = {
+	text: string
+	startBackspacing: boolean
+	onFinish: () => void
+	isFinished: boolean
+}
+
+/** BackspacingText component that backspaces out the given text */
+export const BackspacingText = ({
+	text,
+	startBackspacing,
+	onFinish,
+	isFinished,
+}: BackspacingTextProps) => {
+	const [currentText, setCurrentText] = useState(text)
+	const [blink, setBlink] = useState(true)
+	const [isBackspacing, setIsBackspacing] = useState(false)
+
+	useEffect(() => {
+		if (!startBackspacing) return
+
+		setTimeout(() => {
+			setIsBackspacing(true)
+		}, 1000)
+
+		if (!isBackspacing) return
+
+		// every 0.075 seconds type another character
+		const interval = setInterval(() => {
+			setCurrentText((prev) => prev.slice(0, prev.length - 1))
+		}, backspaceSpeed)
+
+		// Finished typing, let the cursor blink for 2 seconds
+		if (currentText.length === 0) {
+			clearInterval(interval)
+			setBlink(true)
+			setTimeout(() => {
+				setIsBackspacing(false)
+				onFinish()
+			}, 1000)
+		}
+
+		return () => clearInterval(interval)
+	}, [currentText, startBackspacing, isBackspacing, onFinish])
+
+	return (
+		<span>
+			{isFinished ? "" : currentText}
+			{startBackspacing && <Cursor blink={blink} />}
+		</span>
+	)
+}
+
+type DynamicTextProps = Omit<TypingTextProps, "isFinished">
+
+export const DynamicText = ({ text, onFinish, startTyping }: DynamicTextProps) => {
+	const [currentText, setCurrentText] = useState(text)
+	const [isChanging, setIsChanging] = useState(false)
+	const [startBackspacing, setStartBackspacing] = useState(false)
+
+	useEffect(() => {
+		if (startTyping) {
+			setIsChanging(true)
+		}
+
+		if (currentText !== text) {
+			setIsChanging(true)
+			setStartBackspacing(true)
+		}
+	}, [text, currentText, startTyping])
+
+	if (startBackspacing) {
+		return (
+			<BackspacingText
+				text={currentText}
+				onFinish={() => {
+					setCurrentText(text)
+					setStartBackspacing(false)
+				}}
+				startBackspacing={true}
+				isFinished={false}
+			/>
+		)
+	}
+
+	return (
+		<TypingText
+			text={currentText}
+			startTyping={isChanging}
+			onFinish={() => {
+				setIsChanging(false)
+				onFinish()
+			}}
+			isFinished={false}
+		/>
 	)
 }
--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,13 +1,57 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { DragItIn } from "../components/dragItIn"
-import { Socials } from "../components/socials"
-import { TypingText } from "../components/typingText"
+import { GitHubIcon, SpotifyIcon, SubstackIcon, XIcon } from "../components/icons"
 import { useAnimationList } from "../lib/AnimationList"
+import { DynamicText, TypingText } from "../components/typingText"
 import { cn } from "../lib/utils"
 
+const Socials = () => {
+	return (
+		<div className="flex h-8 gap-8 md:h-10">
+			<DragItIn id="Intro Socials github">
+				<a
+					// className={cn(visibleIcons > 0 ? "visible" : "hidden")}
+					href="https://github.com/work-in-progress-danny/"
+				>
+					<GitHubIcon />
+				</a>
+			</DragItIn>
+
+			<DragItIn id="Intro Socials X">
+				<a
+					// className={cn(visibleIcons > 1 ? "visible" : "hidden")}
+					href="https://x.com/w_i_p_danny"
+				>
+					<XIcon />
+				</a>
+			</DragItIn>
+
+			<DragItIn id="Intro Socials substack">
+				<a
+					// className={cn(visibleIcons > 2 ? "visible" : "hidden")}
+					href="https://substack.com/w_i_p_danny"
+				>
+					<SubstackIcon />
+				</a>
+			</DragItIn>
+
+			<DragItIn id="Intro Socials spotify">
+				<a
+					// className={cn(visibleIcons > 3 ? "visible" : "hidden")}
+					href="https://spotify.com/w_i_p_danny"
+				>
+					<SpotifyIcon />
+				</a>
+			</DragItIn>
+		</div>
+	)
+}
+
 const Heading = () => {
-	const { addSelfToAnimationList, onFinish, isAnimating, isFinished } =
+	const { addSelfToAnimationList, onFinish, getIsAnimating, getIsFinished } =
 		useAnimationList("intro header")
+	const isAnimating = getIsAnimating()
+	const isFinished = getIsFinished()
 
 	useEffect(() => {
 		addSelfToAnimationList()
@@ -16,18 +60,35 @@ const Heading = () => {
 	return (
 		<h1 className="title text-5xl self-center md:text-6xl">
 			<TypingText
-				isFinished={isFinished()}
+				isFinished={isFinished}
 				text="WORK IN PROGRESS DANNY"
-				onFinish={() => onFinish()}
-				startTyping={isAnimating()}
+				onFinish={onFinish}
+				startTyping={isAnimating}
 			/>
 		</h1>
 	)
 }
 
+const intros = [
+	"Always in motion, never ending",
+	"What's next?",
+	"Only a place in time",
+	"Expect change, welcome it",
+]
+
 const IntroText = () => {
-	const { addSelfToAnimationList, onFinish, isAnimating, isFinished } =
+	const { addSelfToAnimationList, onFinish, getIsAnimating, getIsFinished } =
 		useAnimationList("intro text")
+	const [currentIntro, setCurrentIntro] = useState(0)
+	const isFinished = getIsFinished()
+	const isAnimating = getIsAnimating()
+
+	const onIntroFinish = () => {
+		if (!isFinished) {
+			onFinish()
+		}
+		setCurrentIntro((prev) => (prev + 1 > intros.length - 1 ? 0 : prev + 1))
+	}
 
 	useEffect(() => {
 		addSelfToAnimationList()
@@ -35,12 +96,7 @@ const IntroText = () => {
 
 	return (
 		<p className="body text-2xl">
-			<TypingText
-				isFinished={isFinished()}
-				text="Always in motion, never finished"
-				onFinish={() => onFinish()}
-				startTyping={isAnimating()}
-			/>
+			<DynamicText text={intros[currentIntro]} onFinish={onIntroFinish} startTyping={isAnimating} />
 		</p>
 	)
 }
